873955f5184bc34e90e57fee9265c5b68a3ab457
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2024-06-25 15:22:06 +0530

    branch-populate

--- routes/customer.route.js
+++ routes/customer.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { DeleteCustomer, SaveCustomer, SignIn, SignInWithMobile, SuperAdminList, UpdateCustomer, ViewCustomer, ViewCustomerById, dueParty, forgetPassword, lockParty, otpVerify, overDueReport, paymentDueReport, saveExcelFile, updatePassword } from "../controller/customer.controller.js";
+import { DeleteCustomer, SaveCustomer, SignIn, SignInWithMobile, SuperAdminList, UpdateCustomer, ViewCustomer, ViewCustomerById, dueParty, forgetPassword, lockParty, otpVerify, overDueReport, paymentDueReport, saveExcelFile, updateExcelFile, updatePassword } from "../controller/customer.controller.js";
 import multer from "multer";
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const upload = multer({ dest: "public/Images/" })
 const uploads = multer({ dest: "public/ExcelFile/" })
 
 router.post("/party-data", uploads.single('file'), saveExcelFile)
+router.post("/update-customer/:database", uploads.single('file'), updateExcelFile)
 
 router.post("/save-customer", upload.any("files"), SaveCustomer);
 router.get("/view-customer/:id/:database", ViewCustomer);
@@ -17,13 +18,13 @@ router.post("/update-customer/:id", upload.any("files"), UpdateCustomer);
 router.post("/login", SignInWithMobile)
 router.post("/signIn", SignIn)
 router.post("/forget-password", forgetPassword)
-router.post("/super-admin-list",SuperAdminList)
+router.post("/super-admin-list", SuperAdminList)
 router.post("/otp-verify", otpVerify)
 router.post("/update-password/:id", updatePassword)
 
 router.get("/due-report/:database", dueParty)
 router.get("/over-due-report/:database", overDueReport)
 router.get("/auto-billing-lock/:database", lockParty)
-router.post("/payment-due-report/:database",paymentDueReport)
+router.post("/payment-due-report/:database", paymentDueReport)
 
 export default router;
