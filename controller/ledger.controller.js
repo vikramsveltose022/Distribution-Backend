da115440253a60d31995f4479081dea6627d02dc
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2024-08-03 16:22:39 +0530

    bulk import

--- controller/ledger.controller.js
+++ controller/ledger.controller.js
@@ -3,18 +3,18 @@ import { Ledger } from "../model/ledger.model.js";
 
 export const viewLedgerByParty = async (req, res, next) => {
     try {
-        const userId = req.params.id;
-        const ledger = await Ledger.find({ partyId: userId, ledgerType: "party" }).sort({ sortorder: -1 }).populate({ path: "partyId", model: "customer" }).populate({ path: "userId", model: "user" })
-        if (!ledger.length > 0) {
-            return res.status(404).json({ message: "Not Found", status: false })
+        const ledger = await Ledger.find({
+            $or: [{ userId: req.params.id }, { partyId: req.params.id }, { expenseId: req.params.id }]
+        }).sort({ sortorder: -1 }).populate({ path: "partyId", model: "customer" }).populate({ path: "userId", model: "user" }).populate({ path: "expenseId", model: "createAccount" });
+        if (ledger.length === 0) {
+            return res.status(404).json({ message: "Not Found", status: false });
         }
-        return res.status(200).json({ Ledger: ledger, status: true })
+        return res.status(200).json({ Ledger: ledger, status: true });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Internal Server Error", status: false });
     }
-    catch (err) {
-        console.log(err);
-        return res.status(500).json({ error: "Internal Server Error", status: false })
-    }
-}
+};
 export const viewLedgerByUser = async (req, res, next) => {
     try {
         const userId = req.params.id;
