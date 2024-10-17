12f2859d6bbaac22950f1c799a9423c7a03c255b
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2024-10-17 14:27:59 +0530

    updated

--- routes/promotion.route.js
+++ routes/promotion.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { SavePromotion, ViewPromotion } from "../controller/promotion.controller.js";
+import { deletePromotion, SavePromotion, UpdatedPromotion, ViewPromotion, ViewPromotionById } from "../controller/promotion.controller.js";
 
 const router = express.Router();
 
 router.post("/save-promotion", SavePromotion);
 router.get("/view-promotion/:id/:database", ViewPromotion)
+router.get("/view-promotion-by-id/:id", ViewPromotionById)
+router.put("/update-promotion/:id", UpdatedPromotion)
+router.delete("/delete-promotion/:id", deletePromotion)
 
 export default router;
