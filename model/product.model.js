d1f2bf4a66fb1c981fb5b391cb3ce13264a0826e
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2024-12-28 15:52:40 +0530

    warehouse

--- model/product.model.js
+++ model/product.model.js
@@ -11,7 +11,7 @@ const ProductSchema = new mongoose.Schema({
         type: String
     },
     partyId: [{
-        paytyId:{
+        partyId:{
             type:String
         },
         purchaseDate:{
