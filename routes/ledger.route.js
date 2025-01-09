c56363e6cddaab44fec92310e0bdeea88468e3a8
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2025-01-09 17:50:50 +0530

    target-updated

--- routes/ledger.route.js
+++ routes/ledger.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { ViewLastLedgerBalance, viewLedgerByParty, viewLedgerByUser } from "../controller/ledger.controller.js";
+import { ViewLastLedgerBalance, viewLedgerByParty, viewLedgerByPartySalesApp, viewLedgerByUser } from "../controller/ledger.controller.js";
 
 const router = express.Router();
 
 router.get("/view-ledger-user/:id", viewLedgerByUser);
 router.get("/view-ledger-party/:id", viewLedgerByParty)
 router.get("/view-last-ledger/:id", ViewLastLedgerBalance)
+router.get("/view-party-ledger-salesapp/:id", viewLedgerByPartySalesApp)
 
 export default router;
