e88f03ddbc9e3157462e0198ba42345596f62190
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2025-02-13 16:20:53 +0530

    updated

--- service/invoice.js
+++ service/invoice.js
@@ -3,34 +3,34 @@ import { DebitNote } from "../model/debitNote.model.js";
 import { CreditNote } from "../model/creditNote.model.js";
 
 export const generateInvoice = async (database) => {
-    const companyDetail = await CompanyDetails.findOne({database:database})
-    if(!companyDetail){
-        return false
-    }
-    if(!companyDetail.dummy){
-        const no = 1
-        const generatedInvoice = `${companyDetail.Prefix}${no.toString().padStart(5, '0')}${companyDetail.Suffix}`;
-        companyDetail.dummy = generatedInvoice;
-        await companyDetail.save();
-        return generatedInvoice
-    } else{
-        const length = companyDetail.Prefix.length
-        const lengths = companyDetail.Suffix.length
-        const first = companyDetail.dummy;
-        const middlePart = first.slice(length, -lengths);
-        const newMiddleNumber = (parseInt(middlePart, 10) + 1).toString().padStart(middlePart.length, "0"); 
-        const generatedInvoice = first.slice(0, length) + newMiddleNumber + first.slice(-lengths);
-        companyDetail.dummy = generatedInvoice;
-        await companyDetail.save();
-        return generatedInvoice
-    }
-    // const companyDetail = await CompanyDetails.findOne({ database: database });
-    // const invoice = companyDetail.dummy + 1;
-    // const generatedInvoice = `${companyDetail.Prefix}${invoice}`;
-    // companyDetail.dummy = invoice;
-    // await companyDetail.save()
+    // const companyDetail = await CompanyDetails.findOne({database:database})
+    // if(!companyDetail){
+    //     return false
+    // }
+    // if(!companyDetail.dummy){
+    //     const no = 1
+    //     const generatedInvoice = `${companyDetail.Prefix}${no.toString().padStart(5, '0')}${companyDetail.Suffix}`;
+    //     companyDetail.dummy = generatedInvoice;
+    //     await companyDetail.save();
+    //     return generatedInvoice
+    // } else{
+    //     const length = companyDetail.Prefix.length
+    //     const lengths = companyDetail.Suffix.length
+    //     const first = companyDetail.dummy;
+    //     const middlePart = first.slice(length, -lengths);
+    //     const newMiddleNumber = (parseInt(middlePart, 10) + 1).toString().padStart(middlePart.length, "0"); 
+    //     const generatedInvoice = first.slice(0, length) + newMiddleNumber + first.slice(-lengths);
+    //     companyDetail.dummy = generatedInvoice;
+    //     await companyDetail.save();
+    //     return generatedInvoice
+    // }
+    const companyDetail = await CompanyDetails.findOne({ database: database });
+    const invoice = companyDetail.dummy + 1;
+    const generatedInvoice = `${companyDetail.Prefix}${invoice}`;
+    companyDetail.dummy = invoice;
+    await companyDetail.save()
 
-    // return generatedInvoice;
+    return generatedInvoice;
 };
 export const generateOrderNo = async (database) => {
     const companyDetail = await CompanyDetails.findOne({ database: database });
